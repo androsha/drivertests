f97c4faca32de41144e717d9e0d3d5e048ad8fad
kate <androkat@mail.ru>
2018-11-13 16:01:09 -0800

    Created solution and 2 projects. Setup .gitignore file. Created login test.

---
+++ DriverAutomation/Driver.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverAutomation
+{
+	internal class Driver
+	{
+		private Driver()
+		{
+		}
+
+		public static IWebDriver Instance = new FirefoxDriver();
+	}
+}
---
+++ Tests/AddExpiredVehicleTest.cs
@@ -0,0 +1,20 @@
+﻿using DriverAutomation;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+	[TestFixture]
+	public class AddExpiredVehicleTest
+	{
+		[Test]
+		public void Test()
+		{
+			LoginPage.Open();
+			LoginPage.Login("", "");
+
+			// todo: replace with correct checks
+			Assert.IsTrue(true);
+		}
+	}
+}
